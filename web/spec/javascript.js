0e199dded05dcc2f38a4ed4a881efd36bef33369
gfis <punctum@punctum.com>
2015-04-25 10:40:27 +0200

    javascript

---
+++ web/spec/javascript.js
@@ -0,0 +1,18 @@
+/*
+	Common JavaScript functions
+	@(#) $Id$
+	2014-11-10: showImage
+	2010-11-26: Dr. Georg Fischer
+*/
+function upper() {
+} // raise
+
+function lower(field) {
+	var result = this.form1.prefix.value.toLowerCase();
+	this.form1.prefix.value = result;
+} // raise
+
+function showImage(imagename, width) {
+	document.write('<img src="' + imagename + '" width="' + width + '" title="' + imagename + '" />');
+} // showImage
+
